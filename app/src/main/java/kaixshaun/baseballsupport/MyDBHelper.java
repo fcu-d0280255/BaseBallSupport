3cc4008894e01eaecf6bfdf275e4f6295b00352e
LAN <blue19970731@gmail.com>
2017-06-21 03:18:32 +0800

    我的app第7次上傳github

--- app/src/main/java/kaixshaun/baseballsupport/MyDBHelper.java
+++ app/src/main/java/kaixshaun/baseballsupport/MyDBHelper.java
@@ -11,17 +11,17 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class MyDBHelper extends SQLiteOpenHelper {
 
     private static final String DBName = "baseballsupport.db";
-    private static int VERSION = 1;
+    private static int VERSION = 2;
     private static SQLiteDatabase database;
 
     public static final String CREATE_Game_TABLE = "CREATE TABLE IF NOT EXISTS Game(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID TEXT NOT NULL,GameName TEXT,HomeTeamID TEXT NOT NULL,AwayTeamID TEXT NOT NULL,HomeScore INTEGER,AwayScore INTEGER)";
-    public static final String CREATE_Team_TABLE = "CREATE TABLE IF NOT EXISTS Team(_id INTEGER PRIMARY KEY AUTOINCREMENT,TeamID,TeamName);";
-    public static final String CREATE_Inning_TABLE = "CREATE TABLE IF NOT EXISTS Inning(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID,Inning,TopHalf,BottonHalf);";
-    public static final String CREATE_BattingOrder_TABLE = "CREATE TABLE IF NOT EXISTS BattingOrder(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID,TeamID,Back,Order,Rule);";
-    public static final String CREATE_Teammate_TABLE = "CREATE TABLE IF NOT EXISTS Teammate(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID,TeamID,Back,S_B);";
-    public static final String CREATE_Record_TABLE = "CREATE TABLE IF NOT EXISTS Record(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID,TeamID,Back,Inning,Round,Order,Situation,Flyto,Out,RBI,Notes)";
-    public static final String CREATE_FinalData_TABLE = "CREATE TABLE IF NOT EXISTS FinalData(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID,TeamID,Back,Order,rule,PA,BA,OBP,Hit,Walk,Error,RBI)";
-    public static final String CREATE_TeamRecord_TABLE = "CREATE TABLE IF NOT EXISTS TeamRecord(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID,TeamID,TotalHit,TotalWalk,TotalError)";
+    public static final String CREATE_Team_TABLE = "CREATE TABLE IF NOT EXISTS Team(_id INTEGER PRIMARY KEY AUTOINCREMENT,TeamID TEXT NOT NULL,TeamName TEXT);";
+    public static final String CREATE_Inning_TABLE = "CREATE TABLE IF NOT EXISTS Inning(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID TEXT NOT NULL,Inning  INTEGER,TopHalf  INTEGER,BottonHalf  INTEGER);";
+    public static final String CREATE_BattingOrder_TABLE = "CREATE TABLE IF NOT EXISTS BattingOrder(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID TEXT NOT NULL,TeamID TEXT NOT NULL,Back  INTEGER,Order  INTEGER,Rule  INTEGER);";
+    public static final String CREATE_Teammate_TABLE = "CREATE TABLE IF NOT EXISTS Teammate(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameIDTEXT NOT NULL,TeamID TEXT NOT NULL,Back  INTEGER,S_B TEXT);";
+    public static final String CREATE_Record_TABLE = "CREATE TABLE IF NOT EXISTS Record(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID TEXT NOT NULL,TeamID TEXT NOT NULL,Back INTEGER,Inning  INTEGER,Round  INTEGER,Order  INTEGER,Situation TEXT,Flyto  INTEGER,Out  INTEGER,RBI  INTEGER,Notes TEXT)";
+    public static final String CREATE_FinalData_TABLE = "CREATE TABLE IF NOT EXISTS FinalData(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID TEXT NOT NULL,TeamID TEXT NOT NULL,Back  INTEGER,Order  INTEGER,rule  INTEGER,PA INTEGER,BA REAL,OBP REAL,Hit INTEGER,Walk INTEGER,Error INTEGER,RBI INTEGER)";
+    public static final String CREATE_TeamRecord_TABLE = "CREATE TABLE IF NOT EXISTS TeamRecord(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID TEXT NOT NULL,TeamID TEXT NOT NULL,TotalHit  INTEGER,TotalWalk  INTEGER,TotalError  INTEGER)";
 
 
     public MyDBHelper(Context context) {
@@ -40,6 +40,7 @@ public class MyDBHelper extends SQLiteOpenHelper {
         db.execSQL(CREATE_Record_TABLE);
         db.execSQL(CREATE_FinalData_TABLE);
         db.execSQL(CREATE_TeamRecord_TABLE);
+
     }
 
     @Override
@@ -57,5 +58,5 @@ public class MyDBHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    
+
 }
