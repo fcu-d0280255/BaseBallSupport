0c2aa66c7cd92a9c55a127a44a4e14eeb02dbb81
LAN <blue19970731@gmail.com>
2017-06-22 03:26:11 +0800

    我的app第8次上傳github

--- app/src/main/java/kaixshaun/baseballsupport/MyDBHelper.java
+++ app/src/main/java/kaixshaun/baseballsupport/MyDBHelper.java
@@ -3,6 +3,7 @@ package kaixshaun.baseballsupport;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * Created by LIN on 2017/6/20.
@@ -11,17 +12,16 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class MyDBHelper extends SQLiteOpenHelper {
 
     private static final String DBName = "baseballsupport.db";
-    private static int VERSION = 2;
-    private static SQLiteDatabase database;
+    private static int VERSION = 3;
 
-    public static final String CREATE_Game_TABLE = "CREATE TABLE IF NOT EXISTS Game(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID TEXT NOT NULL,GameName TEXT,HomeTeamID TEXT NOT NULL,AwayTeamID TEXT NOT NULL,HomeScore INTEGER,AwayScore INTEGER)";
-    public static final String CREATE_Team_TABLE = "CREATE TABLE IF NOT EXISTS Team(_id INTEGER PRIMARY KEY AUTOINCREMENT,TeamID TEXT NOT NULL,TeamName TEXT);";
-    public static final String CREATE_Inning_TABLE = "CREATE TABLE IF NOT EXISTS Inning(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID TEXT NOT NULL,Inning  INTEGER,TopHalf  INTEGER,BottonHalf  INTEGER);";
-    public static final String CREATE_BattingOrder_TABLE = "CREATE TABLE IF NOT EXISTS BattingOrder(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID TEXT NOT NULL,TeamID TEXT NOT NULL,Back  INTEGER,Order  INTEGER,Rule  INTEGER);";
-    public static final String CREATE_Teammate_TABLE = "CREATE TABLE IF NOT EXISTS Teammate(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameIDTEXT NOT NULL,TeamID TEXT NOT NULL,Back  INTEGER,S_B TEXT);";
-    public static final String CREATE_Record_TABLE = "CREATE TABLE IF NOT EXISTS Record(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID TEXT NOT NULL,TeamID TEXT NOT NULL,Back INTEGER,Inning  INTEGER,Round  INTEGER,Order  INTEGER,Situation TEXT,Flyto  INTEGER,Out  INTEGER,RBI  INTEGER,Notes TEXT)";
-    public static final String CREATE_FinalData_TABLE = "CREATE TABLE IF NOT EXISTS FinalData(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID TEXT NOT NULL,TeamID TEXT NOT NULL,Back  INTEGER,Order  INTEGER,rule  INTEGER,PA INTEGER,BA REAL,OBP REAL,Hit INTEGER,Walk INTEGER,Error INTEGER,RBI INTEGER)";
-    public static final String CREATE_TeamRecord_TABLE = "CREATE TABLE IF NOT EXISTS TeamRecord(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID TEXT NOT NULL,TeamID TEXT NOT NULL,TotalHit  INTEGER,TotalWalk  INTEGER,TotalError  INTEGER)";
+    public static final String CREATE_Game_TABLE = "CREATE TABLE IF NOT EXISTS Game (_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID TEXT NOT NULL,GameName TEXT,HomeTeamID TEXT NOT NULL,AwayTeamID TEXT NOT NULL,HomeScore INTEGER,AwayScore INTEGER)";
+    public static final String CREATE_Team_TABLE = "CREATE TABLE IF NOT EXISTS Team (_id INTEGER PRIMARY KEY AUTOINCREMENT,TeamID TEXT NOT NULL,TeamName TEXT)";
+    //public static final String CREATE_Inning_TABLE = "CREATE TABLE IF NOT EXISTS Inning(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID TEXT NOT NULL,Inning  INTEGER,TopHalf  INTEGER,BottonHalf  INTEGER);";
+    public static final String CREATE_BattingOrder_TABLE = "CREATE TABLE IF NOT EXISTS BattingOrder (_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID TEXT NOT NULL,TeamID TEXT NOT NULL,Back,Order,Rule)";
+    public static final String CREATE_Teammate_TABLE = "CREATE TABLE IF NOT EXISTS Teammate (_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID TEXT NOT NULL,TeamID TEXT NOT NULL,Back,S_B)";
+    //public static final String CREATE_Record_TABLE = "CREATE TABLE IF NOT EXISTS Record(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID TEXT NOT NULL,TeamID TEXT NOT NULL,Back INTEGER,Inning  INTEGER,Round  INTEGER,Order  INTEGER,Situation TEXT,Flyto  INTEGER,Out  INTEGER,RBI  INTEGER,Notes TEXT)";
+    //public static final String CREATE_FinalData_TABLE = "CREATE TABLE IF NOT EXISTS FinalData(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID TEXT NOT NULL,TeamID TEXT NOT NULL,Back  INTEGER,Order  INTEGER,rule  INTEGER,PA INTEGER,BA REAL,OBP REAL,Hit INTEGER,Walk INTEGER,Error INTEGER,RBI INTEGER)";
+    //public static final String CREATE_TeamRecord_TABLE = "CREATE TABLE IF NOT EXISTS TeamRecord(_id INTEGER PRIMARY KEY AUTOINCREMENT,GameID TEXT NOT NULL,TeamID TEXT NOT NULL,TotalHit  INTEGER,TotalWalk  INTEGER,TotalError  INTEGER)";
 
 
     public MyDBHelper(Context context) {
@@ -34,28 +34,31 @@ public class MyDBHelper extends SQLiteOpenHelper {
 
         db.execSQL(CREATE_Game_TABLE);
         db.execSQL(CREATE_Team_TABLE);
-        db.execSQL(CREATE_Inning_TABLE);
+        //db.execSQL(CREATE_Inning_TABLE);
         db.execSQL(CREATE_BattingOrder_TABLE);
         db.execSQL(CREATE_Teammate_TABLE);
-        db.execSQL(CREATE_Record_TABLE);
-        db.execSQL(CREATE_FinalData_TABLE);
-        db.execSQL(CREATE_TeamRecord_TABLE);
+        //db.execSQL(CREATE_Record_TABLE);
+        //db.execSQL(CREATE_FinalData_TABLE);
+        //db.execSQL(CREATE_TeamRecord_TABLE);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
-        db.execSQL(" DROP TABLE IF EXISTS Game");
-        db.execSQL(" DROP TABLE IF EXISTS Team");
-        db.execSQL(" DROP TABLE IF EXISTS Inning");
-        db.execSQL(" DROP TABLE IF EXISTS BattingOrder");
-        db.execSQL(" DROP TABLE IF EXISTS Teammate");
-        db.execSQL(" DROP TABLE IF EXISTS Record");
-        db.execSQL(" DROP TABLE IF EXISTS FinalData");
-        db.execSQL(" DROP TABLE IF EXISTS TeamRecord ");
-
+        Log.v("onUpgrade",oldVersion+">>>"+newVersion);
+        //db.execSQL("DROP DATABASE"+DBName);
+        db.execSQL("DROP TABLE IF EXISTS Game");
+        db.execSQL("DROP TABLE IF EXISTS Team");
+        //db.execSQL(" DROP TABLE IF EXISTS Inning");
+        //db.execSQL("DROP TABLE IF EXISTS BattingOrder");
+        //db.execSQL("DROP TABLE IF EXISTS Teammate");
+        //db.execSQL(" DROP TABLE IF EXISTS Record");
+        //db.execSQL(" DROP TABLE IF EXISTS FinalData");
+       // db.execSQL(" DROP TABLE IF EXISTS TeamRecord ");
+        db.close();
         onCreate(db);
+
     }
 
 
